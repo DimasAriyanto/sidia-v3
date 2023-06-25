326027f4eafd5d6c83ec05cbbb810aa4dd8c7fc9
syahidnurrohim <syahidnurrohim@gmail.com>
2023-06-26 00:17:22 +0700

    feat: add barang table migration

--- app/Models/Master/Barang.php
+++ app/Models/Master/Barang.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+
+    protected $table = 'barang';
+
+    protected $fillable = [
+        'nama',
+        'harga',
+        'satuan',
+        'stok',
+    ];
 }
