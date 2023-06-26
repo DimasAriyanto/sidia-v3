f3f499533791d810bad0373180afef0854067ca6
syahidnurrohim <syahidnurrohim@gmail.com>
2023-06-26 11:23:51 +0700

    feat: add views to barang controller

--- app/Http/Controllers/Master/BarangController.php
+++ app/Http/Controllers/Master/BarangController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Master;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class BarangController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('master.barang.index');
+    }
+
+    public function show(int $id)
+    {
+        return view('master.barang.show');
+    }
+
+    public function create()
+    {
+        return view('master.barang.create');
+    }
+
+    public function edit(int $id)
+    {
+        return view('master.barang.edit');
+    }
 }
--- tests/Feature/BarangControllerTest.php
+++ tests/Feature/BarangControllerTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use App\Models\Master\Barang;
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -10,9 +10,26 @@ class BarangControllerTest extends TestCase
 {
     use DatabaseTransactions;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->actingAs(User::factory()->admin()->create());
+    }
+
     public function test_it_can_render_views(): void
     {
-        $barang = Barang::factory()->create();
-        dd($barang);
+        $response = $this->get(route('dashboard.master.barang.index'));
+        $response->assertViewIs('master.barang.index');
+
+        $response = $this->get(route('dashboard.master.barang.create'));
+        $response->assertViewIs('master.barang.create');
+
+        $user = User::factory()->admin()->create();
+
+        $response = $this->get(route('dashboard.master.barang.show', ['id' => $user->id]));
+        $response->assertViewIs('master.barang.show');
+
+        $response = $this->get(route('dashboard.master.barang.edit', ['id' => $user->id]));
+        $response->assertViewIs('master.barang.edit');
     }
 }
